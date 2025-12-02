d0c844a93468b6b59ee7b30cef2daa845f00c45b
abdullashaikhrizvi <abdullashaikh@eng.rizvi.edu.in>
2025-12-02 19:34:38 +0530

    Experiment 8 [1A].c

---
+++ Experiment 8 [1A].c
@@ -0,0 +1,28 @@
+/*
+NAME:Abdulla Anis Ahmed Shaikh
+UIN:251M013
+CLASS:FE MECHANICAL
+DIV:F
+*/
+
+#include <stdio.h>
+
+int main() {
+	char str[100];
+	int i = 0;
+
+	printf("Enter a string: ");
+	fgets(str, sizeof(str), stdin);
+
+	while (str[i] != '\0') {
+		i++;
+	}
+
+	if (str[i-1] == '\n') {
+		i--;
+	}
+
+	printf("Length of string = %d\n", i);
+
+	return 0;
+}
