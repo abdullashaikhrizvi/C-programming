283c43924f26f381194965cd27468a039c0b5c4b
abdullashaikhrizvi <abdullashaikh@eng.rizvi.edu.in>
2025-12-02 18:57:49 +0530

    Create Experiment 1 (1A).c

---
+++ Experiment 1 (1A).c
@@ -0,0 +1,18 @@
+ * /
+ * Name: Abdulla Anis Ahmed Shaikh
+ * UIN: 251M013
+ * Class:FE/Mechanical
+ * Div:F
+ * /
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Name : Abdulla Anis Ahmed Shaikh \n");
+    printf("Age : 18 years \n");
+    printf("Class : FE/Mechanical \n");
+    printf("Division : F\n");
+    printf("UIN : 251M013\n");
+return 0;
+}
